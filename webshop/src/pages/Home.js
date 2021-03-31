748f926382ca42b197b7e1f38b5a42b6afd7af6b
Lisa <lisastina.hansson@gmail.com>
2021-03-31 18:31:37 +0200

    Change number of displayed products on home page

--- webshop/src/pages/Home.js
+++ webshop/src/pages/Home.js
@@ -1,5 +1,6 @@
 import ProductList from '../components/ProductList';
 import style from '../css/Home.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
     return ( 
@@ -12,6 +13,9 @@ const Home = () => {
                     <button className="btn-sm">Learn more</button>
                 </div>
                 <ProductList />
+                <div className={style.seeAll}>
+                    <NavLink exact to="/about">View all items</NavLink>
+                </div>
             </div>
         </div>
      );
