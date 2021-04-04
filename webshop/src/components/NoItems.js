12dc19f15d8e063489855f1ee69c9472554e7447
Lisa <lisastina.hansson@gmail.com>
2021-04-04 19:56:43 +0200

    added empty shopping bag img

--- webshop/src/components/NoItems.js
+++ webshop/src/components/NoItems.js
@@ -6,6 +6,7 @@ const NoItems = () => {
 
     return ( 
         <div className={style.noItems}>
+            <img src="/assets/icons/empty-cart-icon.png" alt="Empty shopping bag"/>
             <h1>Your shopping bag is empty...</h1>
             <p>Take a look at some of my products!</p>
             <button onClick={() => {
