bbee7450a8970df8d4511b91850aa543530d9504
Lisa Hansson <lisastina.hansson@gmail.com>
2021-07-05 17:32:30 +0200

    Layout on confirmation page

--- webshop/src/pages/Confirmation.js
+++ webshop/src/pages/Confirmation.js
@@ -1,9 +1,35 @@
-const Confitmation = () => {
+import { useContext, useEffect, useState } from 'react'
+import { CartContext } from '../contexts/CartContext';
+import style from '../css/Confirmation.module.css';
+
+const Confirmation = () => {
+  const { order } = useContext(CartContext);
+
   return ( 
-    <div>
+    <div className={style.confirmation}>
+      {order &&
+      <div className={style.order}>
+        <h1>Thank you for your order!</h1>
+        <hr />
+        <div className={style.orderInfo}>
+
+        <div className={style.items}>
+        {order.cartItems && order.cartItems.map((item, i) => 
+            <p key={i}>{item.quantity} x {item.name} {item.productType} {item.size && item.size}</p>       
+        )}
+        </div>
+        <div className={style.shippingInfo}>
+          <p>Lisa Hansson <br /> Lindblomsvägen 8 <br />47542 Hönö</p>
+          <p>lisastina.hansson@gmail.com <br />0733450892</p>
+          <p>Payment method: Card</p>
+          <h3>Total price: 234 kr</h3>
+        </div>
+        </div>
+      </div>
+      }
       
     </div>
    );
 }
  
-export default Confitmation;
+export default Confirmation;
