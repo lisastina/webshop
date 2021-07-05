e6f79df979d28ba6b007934780eec52087c92e2c
Lisa Hansson <lisastina.hansson@gmail.com>
2021-07-05 13:01:24 +0200

    Different message depending on selected payment

--- webshop/src/components/PlaceOrder.js
+++ webshop/src/components/PlaceOrder.js
@@ -1,6 +1,12 @@
 import style from '../css/PlaceOrder.module.css';
+import { useState } from 'react';
 
 const PlaceOrder = () => {
+
+  const [payment, setPayment] = useState("Card");
+
+  const paymentOptions = ["Card", "Invoice", "Paypal"]
+
   return ( 
     <div className={style.placeOrder}>
       <div className={style.shippingInfo}>
@@ -29,22 +35,31 @@ const PlaceOrder = () => {
       <div className={style.paymentInfo}>
         <h2>Payment</h2>
         <form>
-          <div className={style.radio}>
-            <input type="radio" id="card" name="payment"/>
+          <div className={style.options}>
+            {paymentOptions.map(option => {
+              return (
+                <div className={style.radio}>
+                  <input type="radio" id={option} name="payment" onClick={()=>setPayment(option)} checked={payment === option}/>
+                  <label htmlFor={option}>{option}</label>
+                </div>
+              )
+            })}
+            {/* <input type="radio" id="card" name="payment" onClick={()=>setPayment("card")} checked={payment === "card"}/>
             <label htmlFor="card">Card</label>
-            <input type="radio" id="invoice" name="payment"/>
+            <input type="radio" id="invoice" name="payment" onClick={()=>setPayment("invoice")}/>
             <label htmlFor="invoice">Invoice</label>
-            <input type="radio" id="paypal" name="payment"/>
+            <input type="radio" id="paypal" name="payment" onClick={()=>setPayment("other")}/>
             <label htmlFor="paypal">Paypal</label>
-            <input type="radio" id="swish" name="payment"/>
-            <label htmlFor="swish">Swish</label>
+            <input type="radio" id="swish" name="payment" onClick={()=>setPayment("other")}/>
+            <label htmlFor="swish">Swish</label> */}
           </div>
         </form>
         <div className={style.selectedPayment}>
+        {payment === "Card" &&
           <form className={style.cardInfo}>
             <div className={style.inputs}>
               <label htmlFor="cardnumber">Cardnumber</label>
-              <input type="text" id={style.cardnumber} name="card"/>
+              <input type="text" id={style.cardnumber}/>
             </div>
             <div className={style.inputsContainer}>
               <div className={style.inputs}>
@@ -56,8 +71,17 @@ const PlaceOrder = () => {
                 <input type="text" id="securitynumber" name="card"/>
               </div>
             </div>
+          </form>}
 
-          </form>
+          {payment === "Invoice" &&
+          <div>
+            <p>An invoice with payment details will be sent to your address.</p>
+          </div>
+          }
+          {payment === "Paypal" && <div>
+            <p>You will be directed to Paypal's page to proceed with your payment after you have place your order.</p>
+          </div>
+          }
         </div>
       </div>
     </div>
