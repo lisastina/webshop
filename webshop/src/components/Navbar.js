e6fc22edad342c39f3a2e1567a422a63bb945b78
Lisa Hansson <lisastina.hansson@gmail.com>
2021-07-08 16:19:26 +0200

    Responsive navbar

--- webshop/src/components/Navbar.js
+++ webshop/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import style from '../css/Navbar.module.css';
 import { NavLink, useHistory, useLocation } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CartContext } from '../contexts/CartContext';
 
 const Navbar = () => {
@@ -8,32 +8,48 @@ const Navbar = () => {
     const location = useLocation();
     const { cartItems: cart, cartLength, setCheckout } = useContext(CartContext);
  
+    const [hamburger, setHamburger] = useState(false);
+
+    const handleClick = () => {
+        setHamburger(!hamburger)
+    }
+
     return ( 
-        <div className={`${style.navbar} ${location.pathname === "/" && style.white}`}>
-            <div className={style.navbarContainer}>
+        <div className={`${style.navbar} ${location.pathname === "/" && !hamburger && style.white}`}>
+            <div className={`${style.navbarContainer} ${hamburger && style.clickedHam}`}>
                 <div className={style.linkContainer}>
+                <div className={style.hamburger} onClick={handleClick}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
                     <div className={style.link}>
-                        <NavLink exact to="/" activeClassName={style.active}>Home</NavLink></div>
+                        <NavLink exact to="/" activeClassName={style.active} onClick={handleClick}>Home</NavLink></div>
                     <div className={style.link}>
-                        <NavLink exact to="/about" activeClassName={style.active}>About</NavLink>
+                        <NavLink exact to="/about" activeClassName={style.active} onClick={handleClick}>About</NavLink>
                     </div>
                     <div className={style.link}>
-                        <NavLink exact to="/products" activeClassName={style.active}>Shop</NavLink>
+                        <NavLink exact to="/products" activeClassName={style.active} onClick={handleClick}>Shop</NavLink>
                     </div>
                 </div>
+                
                 <div className={style.pageTitle}>
-                    <h1 onClick={() => history.push('/')}>LisaStina</h1>
+                    <h1 onClick={() => {
+                        history.push('/')
+                        setHamburger(false)
+                        }}>LisaStina</h1>
                 </div>
                 <div className={style.cartIconWrapper}>
                     <div className={style.cartIcon}onClick={() => {
                     history.push('/checkout')
                     setCheckout(false)
+                    setHamburger(false)
                 }}>
                         <div className={style.cartNumWrapper}>
                             {cart.length > 0 &&<span className={style.cartNumber}>{cartLength}</span>}
                         </div>
                         
-                        {location.pathname === "/" ?
+                        {location.pathname === "/" && !hamburger ?
                         <img src="/assets/icons/cart-icon-white.png" alt="shopping cart icon"/> 
                         : 
                         <img src="/assets/icons/cart-icon.png" alt="shopping cart icon"/>
