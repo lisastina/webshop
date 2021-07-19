30301d74bf0819b194d36f783b4042c6f2ed3458
Lisa Hansson <lisastina.hansson@gmail.com>
2021-07-19 17:39:07 +0200

    Format files

--- webshop/src/components/ProductCard.js
+++ webshop/src/components/ProductCard.js
@@ -1,27 +1,37 @@
-import style from '../css/ProductCard.module.css';
-import { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import { ProductContext } from '../contexts/ProductContext';
+import style from "../css/ProductCard.module.css";
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { ProductContext } from "../contexts/ProductContext";
 
 const ProductCard = (props) => {
+  const history = useHistory();
+  const { viewProduct } = useContext(ProductContext);
 
-    const history = useHistory();
-    const { viewProduct } = useContext(ProductContext);
+  const isOdd = (num) => {
+    return num % 2;
+  };
 
-    const isOdd = (num) => { return num % 2;}
+  return (
+    <div
+      className={`${style.productCard} ${isOdd(props.index) && style.reverse}`}
+    >
+      <div className={style.imgWrapper}>
+        <img
+          src={props.product.img}
+          alt={`${props.product.name} ${props.product.productType}`}
+        />
+      </div>
+      <div className={style.desc}>
+        <h2>
+          {props.product.name} {props.product.productType}
+        </h2>
+        <p>{props.product.desc}</p>
+        <button onClick={() => viewProduct(props.product, history)}>
+          Purchase item
+        </button>
+      </div>
+    </div>
+  );
+};
 
-    return ( 
-        <div className={`${style.productCard} ${isOdd(props.index) && style.reverse}`}>
-            <div className={style.imgWrapper}>
-                <img src={props.product.img} alt={`${props.product.name} ${props.product.productType}`}/>
-            </div>
-            <div className={style.desc}>
-                <h2>{props.product.name} {props.product.productType}</h2>
-                <p>{props.product.desc}</p>
-                <button onClick={() => viewProduct(props.product, history)}>Purchase item</button>
-            </div>
-        </div>
-     );
-}
- 
 export default ProductCard;
