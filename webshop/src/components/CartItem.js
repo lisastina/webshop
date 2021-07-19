918c35c1de52db6be4cb89f94ef064b2d5237f63
Lisa Hansson <lisastina.hansson@gmail.com>
2021-07-19 16:22:14 +0200

    Remove error and console log

--- webshop/src/components/CartItem.js
+++ webshop/src/components/CartItem.js
@@ -1,60 +1,72 @@
 /* eslint-disable */
-import style from '../css/CartItem.module.css';
-import { useContext, useState, useEffect } from 'react'
-import { CartContext } from '../contexts/CartContext';
+import style from "../css/CartItem.module.css";
+import { useContext, useState, useEffect } from "react";
+import { CartContext } from "../contexts/CartContext";
 
 const CartItem = (props) => {
-    const { removeFromCart, changeQuantity, checkout } = useContext(CartContext);
-    const [product, setProduct] = useState(props.item)
+  const { removeFromCart, changeQuantity, checkout } = useContext(CartContext);
+  const [product, setProduct] = useState(props.item);
 
-    const handleQuantity = (e) => {
-        setProduct({...product, quantity: Number(e.target.value)})
-        changeQuantity(props.index, Number(e.target.value))
-    }
+  const handleQuantity = (e) => {
+    setProduct({ ...product, quantity: Number(e.target.value) });
+    changeQuantity(props.index, Number(e.target.value));
+  };
 
-    const price = (value) => {
-        value = value * props.item.quantity
-        return value
-    }
+  const price = (value) => {
+    value = value * props.item.quantity;
+    return value;
+  };
 
-    useEffect(() => {
-        setProduct({...product, quantity: props.item.quantity})
-    }, [props]);
+  useEffect(() => {
+    setProduct({ ...product, quantity: props.item.quantity });
+  }, [props]);
 
-    return ( 
-        <div className={`${style.cartItem} ${checkout && style.checkingOut}`}>
-            <div className={style.imgContainer}>
-                <div className={style.x} onClick={() =>{ 
-                    console.log("click");
-                    removeFromCart(props.item)
-                    }}>
-                        <span></span>
-                        <span></span>
-                    </div>
-                <div className={style.imgWrapper}>
-                    <img src={props.item.img} alt={props.item.name}/>
-                </div>
-                <div className={style.title}>
-                    <h2>{props.item.name} {props.item.productType}</h2>
-                {props.item.size && <p>{props.item.size} cm</p>}
-                </div>
-                
-            </div>
-            <div className={style.info}>
-            <div className={style.quantity}>
-                <label htmlFor="">QTY:</label>
-                {checkout ? 
-                    <div>
-                        <span>{props.item.quantity}</span>
-                    </div> 
-                :
-                    <input onChange={handleQuantity} value={product.quantity} type="number" min="1" step="1" id={props.item.name}/>
-                            }
-            </div>
-                <div><h2>{price(props.item.price)} kr</h2></div>
-            </div>
+  return (
+    <div className={`${style.cartItem} ${checkout && style.checkingOut}`}>
+      <div className={style.imgContainer}>
+        <div
+          className={style.x}
+          onClick={() => {
+            removeFromCart(props.item);
+          }}
+        >
+          <span></span>
+          <span></span>
         </div>
-     );
-}
- 
+        <div className={style.imgWrapper}>
+          <img src={props.item.img} alt={props.item.name} />
+        </div>
+        <div className={style.title}>
+          <h2>
+            {props.item.name} {props.item.productType}
+          </h2>
+          {props.item.size && <p>{props.item.size} cm</p>}
+        </div>
+      </div>
+      <div className={style.info}>
+        <div className={style.quantity}>
+          <label htmlFor="">QTY:</label>
+          {checkout ? (
+            <div>
+              <span>{props.item.quantity}</span>
+            </div>
+          ) : (
+            <input
+              onChange={handleQuantity}
+              value={product.quantity}
+              type="number"
+              min="1"
+              step="1"
+              id={props.item.name}
+            />
+          )}
+        </div>
+        <div>
+          <h2>{price(props.item.price)} kr</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default CartItem;
--- webshop/src/pages/Checkout.js
+++ webshop/src/pages/Checkout.js
@@ -1,53 +1,52 @@
-import style from '../css/Checkout.module.css';
-import { useContext, useEffect } from 'react'
-import { CartContext } from '../contexts/CartContext';
-import CartItem from '../components/CartItem';
-import NoItems from '../components/NoItems';
-import PlaceOrder from '../components/PlaceOrder';
+import style from "../css/Checkout.module.css";
+import { useContext, useEffect } from "react";
+import { CartContext } from "../contexts/CartContext";
+import CartItem from "../components/CartItem";
+import NoItems from "../components/NoItems";
+import PlaceOrder from "../components/PlaceOrder";
 
 const Checkout = () => {
+  const { cartItems, cartTotal, checkout, setCheckout } =
+    useContext(CartContext);
 
-    const { cartItems, cartTotal, checkout, setCheckout} = useContext(CartContext);
+  useEffect(() => {
+    setCheckout(false);
+  }, [setCheckout]);
 
-    useEffect(() => {
-        setCheckout(false);
-    }, [setCheckout]);
+  const handleCheckout = (e) => {
+    e.preventDefault();
+    setCheckout(!checkout);
+  };
 
-    const handleCheckout = (e) => {
-        e.preventDefault()
-        setCheckout(!checkout)
-    }
-
-    return ( 
-        <div>
-            {cartItems.length ? 
-            <form action="">
-            <div className={style.checkout}>
-                {checkout ? <h1>Checkout</h1> : <h1>Shopping cart</h1>}
-                {cartItems && cartItems.map((item, index) => 
-                <div key={index}>
-                    <hr/>
-                    <CartItem item={item} index={index}/>
-                </div>
-                )}
-                <hr/>
-                {checkout ? <PlaceOrder /> :
-                
-                <div className={`${style.buy}`}>
-                    <div>
-                    <h2>Subtotal {cartTotal} kr</h2>
-                    </div>
-                    <button onClick={handleCheckout}>Checkout</button>
-                </div>
-                }
-                
+  return (
+    <div>
+      {cartItems.length ? (
+        <div className={style.checkout}>
+          {checkout ? <h1>Checkout</h1> : <h1>Shopping cart</h1>}
+          {cartItems &&
+            cartItems.map((item, index) => (
+              <div key={index}>
+                <hr />
+                <CartItem item={item} index={index} />
+              </div>
+            ))}
+          <hr />
+          {checkout ? (
+            <PlaceOrder />
+          ) : (
+            <div className={`${style.buy}`}>
+              <div>
+                <h2>Subtotal {cartTotal} kr</h2>
+              </div>
+              <button onClick={handleCheckout}>Checkout</button>
             </div>
-            </form>
-            : <NoItems />
-            }
-            
+          )}
         </div>
-     );
-}
- 
+      ) : (
+        <NoItems />
+      )}
+    </div>
+  );
+};
+
 export default Checkout;
