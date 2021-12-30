bbbb618d6d8bce51f3964bc38bd182bbcd6723a3
Lisa Hansson <lisastina.hansson@gmail.com>
2021-12-30 16:18:26 +0100

    useGetDoc hook

---
+++ webshop/src/hooks/useGetDoc.js
@@ -0,0 +1,14 @@
+import { useFirestoreDocumentData } from "@react-query-firebase/firestore";
+import { collection, doc } from "firebase/firestore";
+import { db } from "../firebase";
+
+const useGetDoc = (col, docName, id) => {
+  const colRef = collection(db, col);
+  const docRef = doc(colRef, id);
+
+  const docQuery = useFirestoreDocumentData([docName, id], docRef);
+
+  return docQuery;
+};
+
+export default useGetDoc;
